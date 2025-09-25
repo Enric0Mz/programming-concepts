a4378a2593dabd90b131161601141179bbe89c05
Enrico Marquez <124639774+Enric0Mz@users.noreply.github.com>
2025-09-25 13:58:17 +0000

    feat: implement bubble_sort (again)

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,19 +1,25 @@
-def bubble_sort(nums_list: list):
-    counter = 1
-    for _ in nums_list:
-        is_sorted = True
-        print(f"Lista no passo {counter}: {nums_list}")
-        counter += 1
-        for j in range(len(nums_list) - 1):
-            if nums_list[j] > nums_list[j+1]:
+def bubble_sort(nums):
+    size = len(nums)
+    is_sorted = True
+
+    for _ in nums:
+        for j in range(size - 1):
+            if nums[j] > nums[j + 1]:
                 is_sorted = False
-                nums_list[j], nums_list[j+1] = nums_list[j+1], nums_list[j]
-        
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
         if is_sorted:
             return
+    
+array1 = [5,4,3,2,1]
+array2 = [5,4,3,2,1]
+array3 = [5,4,3,2,1]
+bubble_sort(array1)
+bubble_sort(array2)
+bubble_sort(array3)
+print(array1)
+print(array2)
+print(array3)
 
-bubble_sort([5,4,3,2,1])
-print("...")
-bubble_sort([1,2,3,5,4])
-print("...")
-bubble_sort([1,2,3,4,5])
+
+# Time complexity O(n^2) - Not the best in sorting algorithms
+# Space complexity O(1) - Really useful in low memory scenarios
