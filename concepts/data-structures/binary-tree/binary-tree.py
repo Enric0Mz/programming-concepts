0284dbe1cffd6e109aad6f40700d1b7bdcf4271d
Enrico Marquez <enricovmarquezz@gmail.com>
2025-09-29 13:04:06 -0300

    feat: implement `BinaryTree` (again)

--- concepts/data-structures/binary-tree/binary-tree.py
+++ concepts/data-structures/binary-tree/binary-tree.py
@@ -1,53 +1,57 @@
 class TreeNode:
-    def __init__(self, value: int):
+    def __init__(self, value: int) -> None:
         self.value = value
         self.left = None
         self.right = None
 
 
 class BinaryTree:
-    def __init__(self):
+    def __init__(self) -> None:
         self.root = None
 
-    def insert(self, value: int):
+    def insert(self, value: int) -> None:
+        node = TreeNode(value)
         if not self.root:
-            self.root = TreeNode(value)
+            self.root = node
         else:
-            self._insert_recursive(self.root, value)
+            self._insert_recursive(self.root, node)
 
-    def _insert_recursive(self, node: TreeNode, value: int):
-        if value < node.value:
-            if node.left:
-                self._insert_recursive(node.left, value)
+    def _insert_recursive(self, node: TreeNode, new_node: TreeNode) -> None:
+        if new_node.value > node.value:
+            if not node.right:
+                node.right = new_node
             else:
-                node.left = TreeNode(value)
+                self._insert_recursive(node.right, new_node)
         else:
-            if node.right:
-                self._insert_recursive(node.right, value)
+            if not node.left:
+                node.left = new_node
             else:
-                node.right = TreeNode(value)
-        
-            
-    def search(self, value: int):
+                self._insert_recursive(node.left, new_node)
+
+    def search(self, value: int) -> bool:
         return self._search_recursive(self.root, value)
     
-    def _search_recursive(self, node: TreeNode, value: int):
+    def _search_recursive(self, node: TreeNode, value: int) -> bool:
         if not node:
             return False
         if node.value == value:
             return True
-        if node.value < value:
+        elif value > node.value:
             return self._search_recursive(node.right, value)
-        return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.left, value)
+
+
     
-
 tree = BinaryTree()
-values_to_insert = [10, 5, 15, 3, 7, 12, 18]
-for val in values_to_insert:
-    tree.insert(val)
 
-print(tree.search(7))
-print(tree.search(14))
-print(tree.search(10))
-print(tree.search(18))
+nums = [5,4,7,2,6,9]
 
+for num in nums:
+    tree.insert(num)
+
+print(tree.root.value)
+
+print("Procurando 4... ", tree.search(4)) # True
+print("Procurando 9... ", tree.search(9)) # True
+print("Procurando 1... ", tree.search(1)) # False  
